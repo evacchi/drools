fc26087a0db634469e24e1b4dd63a85a2d85f23d
Edoardo Vacchi <uncommonnonsense@gmail.com>
2018-12-28 20:53:26 +0100

    wip

--- drools-core/src/main/java/org/kie/api2/RuleUnitInstance.java
+++ drools-core/src/main/java/org/kie/api2/RuleUnitInstance.java
@@ -1,8 +1,30 @@
 package org.kie.api2;
 
+import org.drools.core.common.InternalAgenda;
+import org.drools.core.impl.InternalKnowledgeBase;
+import org.kie.api.runtime.rule.AgendaFilter;
+import org.kie.api2.api.RuleUnit;
 import org.kie.api2.api.UnitInstance;
 
-public class RuleUnitInstance<T> implements UnitInstance<T> {
+public class RuleUnitInstance<T extends RuleUnit> implements UnitInstance<T> {
+
+    private RuleUnit unit;
+    private InternalKnowledgeBase kBase;
+    private InternalAgenda agenda;
+
+    public RuleUnitInstance(RuleUnit unit, InternalKnowledgeBase kBase, InternalAgenda agenda) {
+        this.unit = unit;
+        this.kBase = kBase;
+        this.agenda = agenda;
+    }
+
+    public int fireAllRules() {
+        return fireAllRules(null, -1);
+    }
+
+    private int fireAllRules(AgendaFilter agendaFilter, int fireLimit) {
+        return this.agenda.fireAllRules(agendaFilter, fireLimit);
+    }
 
     public void start() {
 
--- drools-core/src/test/java/org/kie/api2/DataSourceTest.java
+++ drools-core/src/test/java/org/kie/api2/DataSourceTest.java
@@ -2,15 +2,15 @@ package org.kie.api2;
 
 import org.junit.Test;
 import org.kie.api.KieBase;
-import org.kie.api.runtime.KieRuntime;
+import org.kie.api.KieServices;
 import org.kie.api.runtime.KieRuntimeFactory;
 import org.kie.api2.api.DataSource;
 import org.kie.api2.api.RuleRuntime;
 import org.kie.api2.api.RuleUnit;
 import org.kie.api2.impl.DataSourceImpl;
-import org.kie.api2.impl.RuleRuntimeImpl;
 
 public class DataSourceTest {
+
     @Test
     public void testDs() {
         DataSource<String> ds = new DataSourceImpl<>();
@@ -21,20 +21,20 @@ public class DataSourceTest {
 
     @Test
     public void testRuleRuntime() {
-        KieBase kieBase = null;
-        UnitExecutor executor = new UnitExecutor(kieBase);
-        DataSource<String> ds = new DataSourceImpl<>();
+        final KieBase kieBase = KieServices.get().newKieClasspathContainer().getKieBase();
+        final UnitExecutor executor = new UnitExecutor(kieBase);
+        final DataSource<String> ds = new DataSourceImpl<>();
         ds.add("foo");
 
-        RuleRuntime ruleRuntime = KieRuntimeFactory.of(kieBase).get(RuleRuntime.class);
-        RuleUnitInstance<MyUnit> ui = ruleRuntime.instanceOf(new MyUnit(ds));
+        final RuleRuntime ruleRuntime = KieRuntimeFactory.of(kieBase).get(RuleRuntime.class);
+        final RuleUnitInstance<MyUnit> ui = ruleRuntime.instanceOf(new MyUnit(ds));
         executor.submit(ui);
 
         executor.run();
     }
 }
 
-class MyUnit implements RuleUnit{
+class MyUnit implements RuleUnit {
 
     private DataSource<String> strings;
 

4aa331361f21e84d2b0dda7edd8fe604452f3f0b
Edoardo Vacchi <uncommonnonsense@gmail.com>
2021-01-18 15:52:43 +0100

    drlx assembler

--- drools-model/drools-model-compiler/src/main/java/org/drools/modelcompiler/drlx/DrlxAssembler.java
+++ drools-model/drools-model-compiler/src/main/java/org/drools/modelcompiler/drlx/DrlxAssembler.java
@@ -1,14 +1,17 @@
 package org.drools.modelcompiler.drlx;
 
 import org.drools.compiler.builder.impl.CompositeKnowledgeBuilderImpl;
-import org.drools.compiler.builder.impl.KnowledgeBuilderImpl;
 import org.drools.compiler.lang.descr.PackageDescr;
 import org.kie.api.internal.assembler.KieAssemblerService;
 import org.kie.api.io.Resource;
 import org.kie.api.io.ResourceConfiguration;
 import org.kie.api.io.ResourceType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DrlxAssembler implements KieAssemblerService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DrlxAssembler.class);
 
     @Override
     public ResourceType getResourceType() {
@@ -17,6 +20,10 @@ public class DrlxAssembler implements KieAssemblerService {
 
     @Override
     public void addResourceAsPackageDescr(Object kbuilder, Resource resource, ResourceType type, ResourceConfiguration configuration) throws Exception {
+        if (!(kbuilder instanceof CompositeKnowledgeBuilderImpl)) {
+            LOGGER.warn("DrlxAssembler is support only for CompositeKnowledgeBuilderImpl. Skipping: {} given", kbuilder.getClass());
+            return;
+        }
         CompositeKnowledgeBuilderImpl kieBuilder = (CompositeKnowledgeBuilderImpl) kbuilder;
         DrlxCompiler drlxCompiler = new DrlxCompiler();
         PackageDescr packageDescr = drlxCompiler.toPackageDescr(resource);
